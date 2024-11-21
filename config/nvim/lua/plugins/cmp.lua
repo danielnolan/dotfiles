5cef3317a6b5df6d4f87c92cae1fb1a924c8265e
Daniel Nolan <danielanolan@gmail.com>
2024-11-21 09:13:04 -0500

    Fix tab not working when no completions

--- config/nvim/lua/plugins/cmp.lua
+++ config/nvim/lua/plugins/cmp.lua
@@ -13,6 +13,23 @@ return {
       }
     },
     config = function()
+      local cmp_status_ok, cmp = pcall(require, "cmp")
+      if not cmp_status_ok then
+        return
+      end
+
+      local snip_status_ok, luasnip = pcall(require, "luasnip")
+      if not snip_status_ok then
+        return
+      end
+
+      require("luasnip/loaders/from_vscode").lazy_load()
+
+      local check_backspace = function()
+        local col = vim.fn.col(".") - 1
+        return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
+      end
+
       local cmp = require('cmp')
       local kind_icons = {
         Text = "󰦨",
@@ -45,7 +62,7 @@ return {
       cmp.setup({
         snippet = {
           expand = function(args)
-            require('luasnip').lsp_expand(args.body)
+            luasnip.lsp_expand(args.body)
           end,
         },
         window = {
