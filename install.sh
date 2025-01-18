fa5ea232c755cf94c69531c6ff8bfa40e36097c8
Daniel Nolan <danielanolan@gmail.com>
2025-01-18 10:14:45 -0500

    Update install script

--- install.sh
+++ install.sh
@@ -4,15 +4,10 @@ if [ -z "$USER" ]; then
   USER=$(id -un)
 fi
 
-mv /workspaces/.codespaces/.persistedshare/dotfiles $HOME/dotfiles
-
 cd $HOME
 
 # Make passwordless sudo work
 export SUDO_ASKPASS=/bin/true
-
-# Change shell to zsh
-sudo chsh "$(id -un)" --shell "/usr/bin/zsh"
 
 # Install ag for fast searching
 sudo apt-get install -y silversearcher-ag
