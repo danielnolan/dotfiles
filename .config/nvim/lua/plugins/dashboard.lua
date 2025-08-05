4b26aad61b98d550d9408af1eb8820706ab878fc
Daniel Nolan <danielanolan@gmail.com>
2025-08-05 10:59:37 -0400

    Update path to dots

--- .config/nvim/lua/plugins/dashboard.lua
+++ .config/nvim/lua/plugins/dashboard.lua
@@ -25,7 +25,7 @@ return {
           {
             desc = ' dotfiles',
             group = 'Number',
-            action = 'Telescope find_files cwd=~',
+            action = 'Telescope find_files cwd=~/',
             key = 'd',
           },
         },
