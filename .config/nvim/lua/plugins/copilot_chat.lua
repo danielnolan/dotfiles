fb6c56902d5427c03c472caa4e6a979db3604166
Daniel Nolan <danielanolan@gmail.com>
2025-08-05 10:58:52 -0400

    Add copilot chat plugin

---
+++ .config/nvim/lua/plugins/copilot_chat.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    lazy = false,
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "nvim-lua/plenary.nvim", branch = "master" },
+    },
+    build = "make tiktoken",
+    opts = {
+      -- See Configuration section for options
+    },
+  },
+}
